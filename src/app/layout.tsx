7701808be0e6d7de6038180d21ef5df59efd0116
MGalvez91 <galvezcifuentes@gmail.com>
2023-09-06 19:33:20 -0600

    layout mod

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import Header from "./../components/Layout/Header";
+import Footer from "./../components/Layout/Footer";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         <ClientProviders>{children}</ClientProviders>
+        <Footer />
       </body>
     </html>
   );
