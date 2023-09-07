a5d70dc73a30fa4a8d220d83c7ff1854ed0d2c39
MGalvez91 <galvezcifuentes@gmail.com>
2023-09-06 19:33:08 -0600

    colors added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        'dark-blue':'#234e60',
+        'light-orange': '#ff6066'
+      }
     },
   },
   plugins: [require('daisyui')],
