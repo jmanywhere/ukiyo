0f8e2f0f28e9f4ee4955324d8d7254e4af6b6955
MGalvez91 <galvezcifuentes@gmail.com>
2023-09-08 20:31:14 -0600

    page hero and features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,16 @@ import Image from "next/image";
 import heroImg from "@/lotties/HeroImageUkiyo.json";
 import heroBackground from "./../assets/images/home.svg";
 import LottieContainer from "@/components/LottieContainer";
+import FeatureCard from "@/components/FeatureCard";
 
 export default function Home() {
+  const lockedValue = 0; //ilustrative pourpuses
+  const tokenPrice = 0; //ilustrative pourpuses
+  const tokenDeployed = 0; //ilustrative pourpuses
+
   return (
-    <main className="min-h-screen  py-24 px-5 flex flex-col items-center w-full">
-      <div className="pb-16 lg:px-8 flex flex-col lg:flex-row items-center max-w-[1440px]">
+    <main className="min-h-screen py-24 flex flex-col items-center w-full">
+      <div className="pb-24 px-5 lg:px-8 flex flex-col lg:flex-row items-center max-w-[1440px]">
         <div className="z-10 lg:order-1">
           <LottieContainer
             options={{
@@ -41,6 +46,35 @@ export default function Home() {
             </a>
           </div>
         </div>
+      </div>
+      <div className="bg-white w-full px-5 flex flex-col items-center">
+        <div className="relative max-w-[1440px] w-full">
+          <div className="flex flex-col items-center md:items-start md:px-8 bg-white rounded-3xl drop-shadow-xl p-6 mb-5 -top-12 absolute w-full">
+            <h2 className="text-2xl text-black">Protocol Stats</h2>
+            <div className="stats stats-vertical md:stats-horizontal bg-white/20">
+              <div className="stat place-items-center py-2">
+                <div className="stat-figure text-primary"></div>
+                <div className="stat-title mb-3">Total Value Locked</div>
+                <div className="stat-value text-black">${lockedValue}</div>
+              </div>
+              <div className="stat place-items-center py-2">
+                <div className="stat-title mb-3">Token Price</div>
+                <div className="stat-value text-black">${tokenPrice}</div>
+              </div>
+
+              <div className="stat place-items-center py-2">
+                <div className="stat-title mb-3">Total Assets Deployed</div>
+                <div className="stat-value text-black">${tokenDeployed}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h2 className="text-black pt-[350px]">Features</h2>
+        <div>
+          <FeatureCard />
+          <FeatureCard />
+        </div>
+        <div className="h-[400px]">content</div>
       </div>
     </main>
   );
---
+++ src/components/FeatureCard.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+const FeatureCard = () => {
+  return <div>Card</div>;
+};
+
+export default FeatureCard;
