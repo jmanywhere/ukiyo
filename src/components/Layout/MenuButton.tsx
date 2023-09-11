df4b5ae80291028816d06eb5b753df485224c3af
MGalvez91 <galvezcifuentes@gmail.com>
2023-09-11 12:51:11 -0600

    hamburger btn changed

--- src/components/Layout/MenuButton.tsx
+++ src/components/Layout/MenuButton.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import classNames from "classnames";
 import Image from "next/image";
+import { CgMenuRightAlt } from "react-icons/cg";
 import LogoRed from "../../assets/images/Logo-red.svg";
 
 type menuProps = {
@@ -30,15 +31,8 @@ const MenuButton = (props: menuProps) => {
           )}
         >
           {/* hamburger icon */}
-          <svg
-            className="swap-off fill-current"
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 512 512"
-          >
-            <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
-          </svg>
+
+          <CgMenuRightAlt className="swap-off fill-current h-[32px] w-[32px]" />
 
           {/* close icon */}
           <svg
