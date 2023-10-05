5ca5a5d4826ffad13e161aa6947834954d66a8ca
MGalvez91 <galvezcifuentes@gmail.com>
2023-10-04 20:13:33 -0600

    header fixes

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -25,31 +25,33 @@ const Header = () => {
   return (
     <header
       className={classNames(
-        "py-4 px-4  fixed top-0 left-0 right-0 z-50 flex justify-center transition duration-300",
-        scrollPosition > 0 ? "bg-white" : ""
+        "pb-4 px-16  fixed top-0 left-0 right-0 z-50 flex justify-center items-center transition duration-300 ",
+        scrollPosition > 32 ? "bg-light-grey-bg pt-4 drop-shadow-xl" : "pt-8"
       )}
     >
       <div className="flex justify-between max-w-[1440px] w-full">
         <LogoSvg
           width={67}
           height={68}
-          fill={scrollPosition > 0 ? "#EF7D7E" : "#fff"}
+          fill={scrollPosition > 32 ? "#EF7D7E" : "#fff"}
         />
         <MenuButton scroll={scrollPosition} />
-        <nav className="hidden lg:flex items-center gap-x-5 px-5 py-2.5 rounded-full bg-light-orange">
-          <a className="text-light-orange bg-white rounded-full px-7 py-3">
-            Home
-          </a>
-          <a className="hover:text-light-orange  hover:bg-white hover:rounded-full px-7 py-3">
-            Features
-          </a>
-          <a className="hover:text-light-orange  hover:bg-white hover:rounded-full px-7 py-3">
-            Docs
-          </a>
-          <a className="hover:text-light-orange  hover:bg-white hover:rounded-full px-7 py-3">
-            FAQ
-          </a>
-        </nav>
+        <div className="hidden lg:flex items-center">
+          <nav className="hidden lg:flex items-center gap-x-2 px-5 py-2 rounded-full bg-light-orange">
+            <a className="text-light-orange bg-white rounded-full px-4 py-0.5">
+              Home
+            </a>
+            <a className="hover:text-light-orange  hover:bg-white hover:rounded-full px-4 py-0.5">
+              Features
+            </a>
+            <a className="hover:text-light-orange  hover:bg-white hover:rounded-full px-4 py-0.5">
+              Docs
+            </a>
+            <a className="hover:text-light-orange  hover:bg-white hover:rounded-full px-4 py-0.5">
+              FAQ
+            </a>
+          </nav>
+        </div>
       </div>
     </header>
   );
