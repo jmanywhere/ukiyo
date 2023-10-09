2dfd93ccd0ee25f008e9ab5f160cf0047c25bb15
Semi Invader <=>
2023-10-08 18:45:53 -0600

    fix input

--- src/components/FortuneCard.tsx
+++ src/components/FortuneCard.tsx
@@ -16,21 +16,19 @@ const FortuneCard = (props: cardProps) => {
       <Image src={img} alt="image" />
       <h3 className="py-2 text-2xl">{title}</h3>
       <p className="text-light-grey">{text}</p>
-      <div className="max-w-[200px]">
-        <div className="join justify-center 203.border-0 py-4 w-full ">
-          <input
-            type="text"
-            className="input join-item rounded-full transparent bg-transparent border-white/20 placeholder:text-light-gray placeholder:text-xs h-auto font-normal  border-r-0"
-            placeholder={placeholder}
-          />
-          <div className="join-item rounded-r-full bg-transparent  border-white/20 p-2 border-l-0 border-[1px] border-l-0">
-            <a
-              className="btn rounded-full font-bold text-sm bg-pink-orange text-black border-[1px] text-white "
-              href={href}
-            >
-              {btnTxt}
-            </a>
-          </div>
+      <div className="join justify-center border-0 py-4 w-full ">
+        <input
+          type="text"
+          className="input join-item rounded-full transparent bg-transparent border-white/20 placeholder:text-light-gray placeholder:text-xs h-auto font-normal border-r-0 w-full"
+          placeholder={placeholder}
+        />
+        <div className="join-item rounded-r-full bg-transparent  border-white/20 p-2 border-l-0 border-[1px]">
+          <a
+            className="btn rounded-full font-bold text-sm bg-pink-orange border-[1px] text-white "
+            href={href}
+          >
+            {btnTxt}
+          </a>
         </div>
       </div>
     </div>
