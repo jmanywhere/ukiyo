49a65b3109c5fdb5642bda4c82f11d0b83040004
Semi Invader <=>
2023-10-12 11:38:47 -0600

    added solical links below logo instead of subscribe button

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -3,16 +3,31 @@ import SuscribeBtn from "./SuscribeBtn";
 
 import { RiTwitterXLine, RiLinkedinFill } from "react-icons/ri";
 import { LiaTelegram } from "react-icons/lia";
+import { HiOutlineMail } from "react-icons/hi";
 
 const Footer = () => {
   return (
-    <footer className="footer-gradient-sm md:footer-gradient pt-16 pb-8 w-full flex flex-col items-center border-0">
+    <footer className="footer-gradient-sm md:footer-gradient pt-16 pb-0 w-full flex flex-col items-center border-0">
       <div className="max-w-[1440px]  flex flex-col items-center w-full">
         <div className="w-full flex flex-col items-center xl:flex-row xl:justify-center xl:items-start xl:px-6">
           <div className="flex flex-col lg:flex-row items-center lg:justify-between px-3  lg:items-start w-full pb-5 md:px-8   max-w-[1440px] ">
             <div className="mb-6 lg:mr-16 flex flex-col items-center">
               <LogoSvg width={88} height={90} fill="white" />
-              <SuscribeBtn />
+              {/* <SuscribeBtn /> */}
+              <div className="flex items-center gap-4 pt-2">
+                <a
+                  className="btn border-none rounded-full font-bold text-lg bg-light-orange text-white hover:bg-light-grey-bg hover:text-secondary"
+                  href={"mailto:hello@ukiyo.network"}
+                >
+                  <HiOutlineMail />
+                </a>
+                <a
+                  className="btn border-none rounded-full font-bold bg-light-orange text-white hover:bg-light-grey-bg hover:text-secondary px-3"
+                  href="https://t.me/ukiyo_protocol"
+                >
+                  <LiaTelegram className="text-xl mr-[1px]" />
+                </a>
+              </div>
             </div>
             {/*Links*/}
             <div className="grid grid-cols-2 md:grid-cols-4 gap-6 place-content-evenly w-full px-6 max-w-[375px] md:max-w-full">
@@ -44,9 +59,9 @@ const Footer = () => {
           </div>
         </div>
         <div className="border-t-white/10 border-t-[1px] w-full flex justify-center ">
-          <div className="max-w-[1440px] w-full flex flex-col lg:flex-row items-center justify-between py-5">
+          <div className="max-w-[1440px] w-full flex flex-col lg:flex-row items-center justify-between py-5 px-2">
             {/*Copyright*/}
-            <p className="text-xs font-normal lg:px-10 text-white">
+            <p className="text-[10px] sm:text-sm font-normal lg:px-10 text-white whitespace-pre-wrap">
               Copyright © 2023 ukiyo Protocol. All rights reserved.
             </p>
             {/*Socials*/}
