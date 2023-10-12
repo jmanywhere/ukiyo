ea066ef109c09382e60cca6c5bd6698b3ee9f025
Semi Invader <=>
2023-10-12 11:38:31 -0600

    tweak contact us buttons

--- src/components/FortuneCard.tsx
+++ src/components/FortuneCard.tsx
@@ -1,5 +1,6 @@
 import Image, { type StaticImageData } from "next/image";
 import { LiaTelegram } from "react-icons/lia";
+import { HiOutlineMail } from "react-icons/hi";
 
 type cardProps = {
   title: string;
@@ -15,19 +16,22 @@ const FortuneCard = (props: cardProps) => {
       <Image src={img} alt="image" />
       <h3 className="pt-6 pb-3 text-2xl text-white">{title}</h3>
       <p className="text-light-grey pb-8">{text}</p>
-      <div className="flex justify-center gap-4">
-        <a
-          className="btn rounded-full font-bold text-lg bg-light-orange text-black border-[1px] text-white"
-          href={emailHref}
-        >
-          Email Us
-        </a>
-        <a
-          className="btn rounded-full font-bold bg-light-orange text-black border-[1px] text-white px-3"
-          href="https://t.me/ukiyo_protocol"
-        >
-          <LiaTelegram className="text-xl mr-[1px]" />
-        </a>
+      <div className="flex flex-col justify-center items-start gap-2">
+        <div>Contact Us:</div>
+        <div className="flex items-center gap-4">
+          <a
+            className="btn rounded-full font-bold text-lg bg-light-orange border-[1px] text-white hover:bg-light-grey-bg hover:text-secondary"
+            href={emailHref}
+          >
+            <HiOutlineMail />
+          </a>
+          <a
+            className="btn rounded-full font-bold bg-light-orange border-[1px] text-white hover:bg-light-grey-bg hover:text-secondary px-3"
+            href="https://t.me/ukiyo_protocol"
+          >
+            <LiaTelegram className="text-xl mr-[1px]" />
+          </a>
+        </div>
       </div>
     </div>
   );
